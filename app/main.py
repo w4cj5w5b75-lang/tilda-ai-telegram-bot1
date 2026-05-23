9349d438f0517ff67978aab73f39f7bfb932499e
AAA1603040404 <w4cj5w5b75@privaterelay.appleid.com>
2026-05-24 00:47:58 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 import asyncio
 import logging
+import os
+import sys
 
-from aiogram import Bot, Dispatcher
+
+# Это нужно, если хостинг запускает файл как /app/app/main.py,
+# а не как python -m app.main
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(CURRENT_DIR)
+
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+
 
 from app.config import settings
 from app.bot_handlers import router
+from aiogram import Bot, Dispatcher
 
 
 async def main():
